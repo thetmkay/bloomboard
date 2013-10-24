7c6575064cca4c595dc11750f5b1c814199b9ad6
George Nishimura <thetmkay@gmail.com>
2013-10-24 13:09:21 +0100

    integrate raphael, jquery and got draw working

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -8,6 +8,32 @@ angular.module('bloomboard.controllers', []).
 
   }).
   controller('BoardCtrl', function ($scope) {
-    // write Ctrl here
-    $scope.boardText = "this is a board";
+
+
+    var paper = new Raphael($('#drawingBoard'));
+    var line_path_string;
+    var mousedown = false;
+
+    var drawMouseDown = function (e) {
+    	line_path_string = "M" + e.clientX + "," + e.clientY;
+    	mousedown = true;
+    };
+
+    var drawMouseUp = function() {
+    	mousedown = false;
+    	console.log(mousedown);
+    };
+
+    var drawMove = function(e) {
+    	if(mousedown){
+	    	line_path_string += "L" + e.clientX + "," + e.clientY;
+	    	var path = paper.path(line_path_string);
+	    	path.attr({stroke:"#000000", "stroke-width":3});   	
+    	}
+    };
+
+    paper.raphael.mousedown(drawMouseDown);
+    paper.raphael.mousemove(drawMove);
+    paper.raphael.mouseup(drawMouseUp);
+
   });
