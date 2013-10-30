068f20ec62a0ac8c8e14842c8f9c1d799d68d075
Thomas Przepiorka <tprzepiorka@ubuntu.(none)>
2013-10-30 17:58:32 +0000

    Remove debug messages

--- routes/mongo_db_lib.js
+++ routes/mongo_db_lib.js
@@ -6,17 +6,12 @@ var saveBoard = function(boardData) {
       MongoClient.connect("mongodb://tom:biscuit@paulo.mongohq.com:10010/app18852387", function(err, db) {
         var collection = db.collection('boards');
 
-        var secondsBefore = new Date().getTime() / 1000;
-
         collection.update({name: "testBoard2"},
                           {$set: {data: boardData }}, {safe:true},
                           function(err, doc) {
             if(err) {
                 console.error(err);
             }
-
-            var secondsAfter = new Date().getTime() / 1000;
-            console.log("Save took ", (secondsAfter - secondsAfter), "seconds");
 
             collection.find({}).toArray(function(err, docs) {
                 if(err){
@@ -31,16 +26,14 @@ var saveBoard = function(boardData) {
   };
 
   var getBoard = function(boardName, callback) {
+
     MongoClient.connect("mongodb://tom:biscuit@paulo.mongohq.com:10010/app18852387", function(err, db) {
-        var secondsBefore = new Date().getTime() / 1000;
 
         var collection = db.collection('boards');
         collection.findOne({name:"testBoard2"}, function(err, doc) {
             if(err) {
                 console.error(err);
             }
-            var secondsAfter = new Date().getTime() / 1000;
-            console.log("Load took ", (secondsAfter - secondsAfter), "seconds");
             callback(doc);
         })
     })
