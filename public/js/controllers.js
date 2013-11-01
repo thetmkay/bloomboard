0abdd841861478ff4c8a719c98c7488ea7dea3ca
Leo Mak <leomak@dyn1234-40.wlan.ic.ac.uk>
2013-11-01 12:18:04 +0000

    added mobile drawing

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -16,26 +16,44 @@ angular.module('bloomboard.controllers', []).
     var mousedown = false;
 
     var drawMouseDown = function (e) {
-    	line_path_string = "M" + e.clientX + "," + e.clientY;
-    	mousedown = true;
+            line_path_string = "M" + e.clientX + "," + e.clientY;
+            mousedown = true;
     };
 
     var drawMouseUp = function() {
-    	mousedown = false;
-    	console.log(mousedown);
+            mousedown = false;
+            console.log(mousedown);
     };
 
     var drawMove = function(e) {
-    	if(mousedown){
-	    	line_path_string += "L" + e.clientX + "," + e.clientY;
-	    	var path = paper.path(line_path_string);
-	    	path.attr({stroke:"#000000", "stroke-width":3});   	
-    	}
+            if(mousedown){
+                    line_path_string += "L" + e.clientX + "," + e.clientY;
+                    var path = paper.path(line_path_string);
+                    path.attr({stroke:"#000000", "stroke-width":3});           
+            }
     };
+    
+   var drawMouseDownTouch = function (e) {
+    
+    	line_path_string = "M" + e.touches[0].pageX + "," + e.touches[0].pageY;
+    	mousedown = true;
+    };
+
+	var drawMoveTouch = function(e) {
+		e.preventDefault();
+   		if(mousedown){
+	    		line_path_string += "L" + e.touches[0].pageX + "," + e.touches[0].pageY;
+	    		var path = paper.path(line_path_string);
+	    		path.attr({stroke:"#000000", "stroke-width":3});   	
+   		}
+   	};
 
     paper.raphael.mousedown(drawMouseDown);
     paper.raphael.mousemove(drawMove);
     paper.raphael.mouseup(drawMouseUp);
+    paper.raphael.touchstart(drawMouseDownTouch);
+    paper.raphael.touchmove(drawMoveTouch);
+    paper.raphael.touchend(drawMouseUp);
 
   }).controller('HomeCtrl', function ($scope) {
 
