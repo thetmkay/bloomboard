db1fd16e56f25a4a77bdc43f543d22e3bcbf30f1
Yufei <yufeixie@hotmail.com>
2013-11-07 07:02:20 +0000

    attempted merging

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -7,13 +7,19 @@ angular.module('bloomboard.controllers', []).
 
 
   }).
-  controller('BoardCtrl', function ($scope, persistenceService) {
+  controller('BoardCtrl', function ($scope, persistenceService, socket) {
 
     $scope.boardText = "this is a board";
     
     var paper = new Raphael($('#drawingBoard'));
     var line_path_string;
     var mousedown = false;
+
+    var sketchpad = Raphael.sketchpad("editor", {
+        width: 400,
+        height: 400,
+        editing: true
+    });
 
     var drawMouseDown = function (e) {
             line_path_string = "M" + e.clientX + "," + e.clientY;
@@ -24,6 +30,7 @@ angular.module('bloomboard.controllers', []).
             mousedown = false;
             console.log(mousedown);
             var json = paper.toJSON();
+
             persistenceService.saveBoard(json);
     };
 
@@ -32,6 +39,8 @@ angular.module('bloomboard.controllers', []).
                     line_path_string += "L" + e.clientX + "," + e.clientY;
                     var path = paper.path(line_path_string);
                     path.attr({stroke:"#000000", "stroke-width":3}); 
+            
+
             }
     };
 
@@ -42,6 +51,20 @@ angular.module('bloomboard.controllers', []).
     paper.raphael.mousedown(drawMouseDown);
     paper.raphael.mousemove(drawMove);
     paper.raphael.mouseup(drawMouseUp);
+
+    socket.on('connect', function() {
+        sketchpad.change(function() {
+            $('#drawingBoard').val(sketchpad.json());
+            socket.emit('draw', $('#drawingBoard').val());
+        });
+    });
+
+// socket listener
+
+    socket.on('update_sketch', function (data) {
+      sketchpad.json(data);
+       $('#drawingBoard').val(sketchpad.json());
+    });
 
   }).controller('HomeCtrl', function ($scope) {
 
--- public/js/services.js
+++ public/js/services.js
@@ -29,4 +29,28 @@ appServicesModule.service('persistenceService', function($http) {
 	};
 	this.boardData = boardData;
 
-})
+});
+
+appServicesModule.factory('socket', function ($rootScope) {
+  var socket = io.connect();
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {  
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+  };
+});
