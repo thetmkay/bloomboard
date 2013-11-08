48857cb3b2bf7339e13700d444d4ca514b9995e1
Niket Shah <masterniket@gmail.com>
2013-11-08 01:10:15 +0000

    added socket.io implementation

--- public/js/directives.js
+++ public/js/directives.js
@@ -44,29 +44,43 @@ module.directive('fittext', ['$window', '$document',
 	}
 ]);
 
-module.directive('bloomboard', function() {
+module.directive('bloomboard', function(socket) {
 	return {
 		restrict: "E",
 		// replace: true,
 		// transclude: true,
-		template: '<div id="topLeft"></div>' +
-		'<div id="bottomRight"></div>' +
-		'<input type="hidden" id="boardData">',
+		template: '<div id="topLeft"></div>' + '<div id="bottomRight"></div>' + '<input type="hidden" id="boardData">',
 		scope: {
 			width: "=",
 			height: "="
 		},
-		compile: function(element, attrs) {
-			console.log("i got here maxFontSize");
+		// compile: function(element, attrs) {
+		// 	console.log("i got here maxFontSize");
+
+
+		// },
+		link: function(scope, element, attrs) {
 			var paper = new Raphael(element);
 			var sketchpad = Raphael.sketchpad(paper, {
-				width: 480,
-				height: 320,
+				width: scope.width,
+				height: scope.height,
 				editing: true
 			});
-		},
-		link: function($scope, $element, $attrs) {
 
+			socket.on('connect', function() {
+				sketchpad.change(function() {
+					console.log("hello");
+					var boardData =  document.querySelector('bloomboard #boardData');
+					boardData.value = sketchpad.json();
+					socket.emit('draw', boardData.value);
+				});
+
+				socket.on('update_sketch', function(data) {
+					console.log('hi');
+					sketchpad.json(data);
+					element.value = sketchpad.json();
+				});
+			});
 		}
 	}
 });
