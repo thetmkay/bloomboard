6c8867b99882f1d1a8b43f0b3bb05f51dae56e16
Niket Shah <masterniket@gmail.com>
2013-11-08 15:16:15 +0000

    modified css to have variable board size, fixed socket, yufei says dynamic

--- public/js/directives.js
+++ public/js/directives.js
@@ -4,75 +4,80 @@
 
 var module = angular.module('bloomboard.directives', [])
 
-module.directive('clickLogin', function () {
+module.directive('clickLogin', function() {
 	return {
 		restrict: 'A',
 		scope: true,
 		replace: true,
-		templateUrl:'partials/login',
-		controller: ['$scope', '$http','$location', 'sessionService', function ($scope, $http, $location, sessionService){  
-		    
-			$scope.showLogin = true;
+		templateUrl: 'partials/login',
+		controller: ['$scope', '$http', '$location', 'sessionService',
+			function($scope, $http, $location, sessionService) {
 
-		    $scope.$watch(function() {
-			    	return sessionService.activeSession;
-			    },
-			    function(newVal) {
-			    	console.log(newVal)
-			    	if(newVal)
-			    		$("#loginModal").modal('hide');
-			    	else
-			    		$("#loginModal").modal('show');
-			    });
+				$scope.showLogin = true;
 
-		    $scope.loginData = function() {
-		    	//add some validation?
-		    	sessionService.login($scope.login);
-		    };
-		    $scope.createUser = function() {
-		    	//add some validation?
-		    	sessionService.register($scope.create);
-		    };
-		  }]
+				$scope.$watch(function() {
+						return sessionService.activeSession;
+					},
+					function(newVal) {
+						console.log(newVal)
+						if (newVal)
+							$("#loginModal").modal('hide');
+						else
+							$("#loginModal").modal('show');
+					});
+
+				$scope.loginData = function() {
+					//add some validation?
+					sessionService.login($scope.login);
+				};
+				$scope.createUser = function() {
+					//add some validation?
+					sessionService.register($scope.create);
+				};
+			}
+		]
 	};
 });
 
 module.directive('bloomboard', function(socket) {
 	return {
 		restrict: "E",
-		 replace: true,
+		// replace: true,
 		// transclude: true,
 		template: '<div id=drawingBoard>' + '<div id="topLeft"></div>' + '<div id="bottomRight"></div>' + '<input type="hidden" id="boardData">' + '</div>',
-		scope: true,
+		scope: {
+			width: "=",
+			height: "="
+		},
 		// compile: function(element, attrs) {
 		// 	console.log("i got here maxFontSize");
 
 
 		// },
 		link: function(scope, element, attrs) {
+			var css = document.createElement("style");
+			css.type = "text/css";
+			css.innerHTML = "#drawingBoard { width: " + scope.width + "px; height: " + scope.height + "px; }";
+			document.body.appendChild(css);
 			var sketchpad = Raphael.sketchpad("drawingBoard", {
-				width: 640,
-				height: 480,
+				width: scope.width,
+				height: scope.height,
 				editing: true
 			});
 
 			socket.on('connect', function() {
 				sketchpad.change(function() {
-					console.log("hello");
-					var boardData = document.querySelector('bloomboard #boardData');
+					var boardData = document.querySelector('#boardData');
 					boardData.value = sketchpad.json();
-
-
-
 					socket.emit('draw', boardData.value);
 				});
 
 			});
 
 			socket.on('update_sketch', function(data) {
-				console.log('hi');
-				sketchpad.json(data, {fireChange: false});
-				// element.value = sketchpad.json();
+				sketchpad.json(data, {
+					fireChange: false
+				});
 			});
 		}
 	}
